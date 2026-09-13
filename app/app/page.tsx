f0b2c1e2113d150aa98704aeb9d212882b358ea7
tonyaloysiusp-cyber <tonyaloysiusp@gmail.com>
2026-09-13 11:56:06 +0400

    Delete app directory

--- app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Magical Touch</h1>
-      <p className="mt-2 text-gray-500">Create. Design. Make It Magical.</p>
-    </main>
-  );
-}
